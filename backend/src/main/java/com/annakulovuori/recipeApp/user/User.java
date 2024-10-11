0528205a3ad12bc8546b13f6dd94b4254abaa394
Anna Kulovuori <anna.kulovuori@tuni.fi>
2024-10-11 14:43:41 +0300

    backend CREATE: new data class User with id, username, email and password properties

---
+++ backend/src/main/java/com/annakulovuori/recipeApp/user/User.java
@@ -0,0 +1,18 @@
+package com.annakulovuori.recipeApp.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    private int id;
+    private String username;
+    private String email;
+    private String password;
+}
