648469a4aa916a96f4ac2fdafc0f16b8d37888d0
Anna Kulovuori <anna.kulovuori@tuni.fi>
2024-10-11 15:14:32 +0300

    backend FEAT: UserEntity class implement UserDetails. Override methods

--- backend/src/main/java/com/annakulovuori/recipeApp/user/UserEntity.java
+++ backend/src/main/java/com/annakulovuori/recipeApp/user/UserEntity.java
@@ -5,6 +5,10 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
 
 
 @Data
@@ -12,7 +16,7 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-public class UserEntity {
+public class UserEntity implements UserDetails {
     //id autoincrement
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -21,4 +25,29 @@ public class UserEntity {
     private String username;
     private String email;
     private String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
 }
