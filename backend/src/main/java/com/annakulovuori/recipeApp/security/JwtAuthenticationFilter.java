af1a359e5a3da01582af0635b45c6101b53e81a4
Anna Kulovuori <anna.kulovuori@tuni.fi>
2024-10-14 16:21:49 +0300

    backend CREATE: new class JwtAuthenticationFilter extending OncePerRequestFilter

---
+++ backend/src/main/java/com/annakulovuori/recipeApp/security/JwtAuthenticationFilter.java
@@ -0,0 +1,13 @@
+package com.annakulovuori.recipeApp.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+}
