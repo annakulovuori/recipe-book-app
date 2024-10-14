733093aca06a071118ca90bd2daacaeb9a86da84
Anna Kulovuori <anna.kulovuori@tuni.fi>
2024-10-14 16:39:32 +0300

    backend FEAT: override doFilterInternal. ADD: Component and RequiredArgsConstructor annotations

--- backend/src/main/java/com/annakulovuori/recipeApp/security/JwtAuthenticationFilter.java
+++ backend/src/main/java/com/annakulovuori/recipeApp/security/JwtAuthenticationFilter.java
@@ -4,10 +4,21 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+@Component
+@RequiredArgsConstructor //creates constructor for all final and nonNull properties
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
+    @Override                        // request, response and filter chain cannot be null
+    protected void doFilterInternal(@NonNull HttpServletRequest request,
+                                    @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain filterChain) throws ServletException, IOException {
+
+    }
 }
