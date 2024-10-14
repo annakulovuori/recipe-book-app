a597a2ca6f89505c92bfdea6f88ec150d9a0ffee
Anna Kulovuori <anna.kulovuori@tuni.fi>
2024-10-14 16:09:40 +0300

    backend CREATE: new interface UserRepository extending JpaReposotory

---
+++ backend/src/main/java/com/annakulovuori/recipeApp/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.annakulovuori.recipeApp.repositories;
+
+import com.annakulovuori.recipeApp.models.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+//in JpaRepository there is already save, findAll etc.
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+}
