2e74e4ac20276be79bb4be4662df63dc3b686524
Anna Kulovuori <anna.kulovuori@tuni.fi>
2024-10-14 16:14:01 +0300

    backend FEAT: impelent findByUsername

--- backend/src/main/java/com/annakulovuori/recipeApp/repositories/UserRepository.java
+++ backend/src/main/java/com/annakulovuori/recipeApp/repositories/UserRepository.java
@@ -4,7 +4,12 @@ import com.annakulovuori.recipeApp.models.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 //in JpaRepository there is already save, findAll etc.
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+
+    Optional<UserEntity> findByUsername(String username);
+
 }
