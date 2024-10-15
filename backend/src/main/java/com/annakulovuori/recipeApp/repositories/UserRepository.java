346e528c478970f856f99eac195fc2ad69dbf9d3
Anna Kulovuori <anna.kulovuori@tuni.fi>
2024-10-15 14:15:59 +0300

    backend REFACTOR: findByUsername method name to findByNickname

--- backend/src/main/java/com/annakulovuori/recipeApp/repositories/UserRepository.java
+++ backend/src/main/java/com/annakulovuori/recipeApp/repositories/UserRepository.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Integer> {
 
-    Optional<UserEntity> findByUsername(String username);
+    Optional<UserEntity> findByNickname(String username);
 
     Optional<UserEntity> findByEmail(String email);
 
