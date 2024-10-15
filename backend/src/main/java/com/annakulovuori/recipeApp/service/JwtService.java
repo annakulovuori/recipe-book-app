8e0776966627a27a745d77df3daa6a5bdf4b48e8
Anna Kulovuori <anna.kulovuori@tuni.fi>
2024-10-15 14:50:05 +0300

    backend CREATE: new method generateToken with extraClaims and userdetails parameters returning Jwts

--- backend/src/main/java/com/annakulovuori/recipeApp/service/JwtService.java
+++ backend/src/main/java/com/annakulovuori/recipeApp/service/JwtService.java
@@ -4,15 +4,32 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.Map;
 import java.util.function.Function;
 
 @Service
 public class JwtService {
 
     private static final String SECRET_KEY = "secret_key";
+
+    public String generateToken(
+            Map<String, Object> extraClaims,
+            UserDetails userdetails
+    ){
+        return Jwts
+                .builder()
+                .claims(extraClaims)
+                .subject(userdetails.getUsername())
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24)) // expires after a day
+                .signWith(getKey())
+                .compact();
+    }
 
 
     //extractUsername actually extracts email now
@@ -25,6 +42,8 @@ public class JwtService {
         final Claims claims = extractAllClaims(token);
         return claimsResolver.apply(claims);
     }
+
+    // Extract all claims from jwt token
     private Claims extractAllClaims(String token) {
         return Jwts.parser()
                 .verifyWith(getKey())
