e401ad7f9256e94267e75c1f44d26fb5eb63c78d
Anna Kulovuori <anna.kulovuori@tuni.fi>
2024-10-17 14:51:34 +0300

    backend CREATE: new bean authenticationManager with AuthenticationConfiguration parameter

--- backend/src/main/java/com/annakulovuori/recipeApp/config/ApplicationConfig.java
+++ backend/src/main/java/com/annakulovuori/recipeApp/config/ApplicationConfig.java
@@ -4,8 +4,10 @@ import com.annakulovuori.recipeApp.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -41,4 +43,8 @@ public class ApplicationConfig {
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
 }
