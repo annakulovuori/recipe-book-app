ac62d2851136e398c600accaed33a4c159998a44
Anna Kulovuori <anna.kulovuori@tuni.fi>
2024-10-17 17:33:04 +0300

    backend FEAT: implement register method, return authenticationResponse. Inject UserRepository, PasswordEncoder, JwtService

--- backend/src/main/java/com/annakulovuori/recipeApp/services/AuthenticationService.java
+++ backend/src/main/java/com/annakulovuori/recipeApp/services/AuthenticationService.java
@@ -1,15 +1,38 @@
 package com.annakulovuori.recipeApp.services;
 
+import com.annakulovuori.recipeApp.models.Role;
+import com.annakulovuori.recipeApp.models.UserEntity;
+import com.annakulovuori.recipeApp.repositories.UserRepository;
 import com.annakulovuori.recipeApp.security.AuthenticationRequest;
 import com.annakulovuori.recipeApp.security.AuthenticationResponse;
 import com.annakulovuori.recipeApp.security.RegisterRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AuthenticationService {
+
+    private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+
+
     public AuthenticationResponse register(RegisterRequest request) {
+        var user = UserEntity.builder()
+                .email(request.getEmail())
+                .nickname(request.getNickname())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+        // onko spostia ja nikkiä olemassa?? tee
+        repository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
     }
 
     public AuthenticationResponse authenticate(AuthenticationRequest request) {
