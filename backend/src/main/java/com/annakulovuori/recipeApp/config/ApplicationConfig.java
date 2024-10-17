4df4b43cc6067b10be7b93c61d55bf99ac477c80
Anna Kulovuori <anna.kulovuori@tuni.fi>
2024-10-17 13:57:54 +0300

    backend CREATE: new class ApplicationConfig with configuration annotation. New bean UserDetailsService. FEAT: implement UserDetailsService bean using findByEmail.

---
+++ backend/src/main/java/com/annakulovuori/recipeApp/config/ApplicationConfig.java
@@ -0,0 +1,23 @@
+package com.annakulovuori.recipeApp.config;
+
+import com.annakulovuori.recipeApp.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationConfig {
+
+    private final UserRepository repository;
+
+    // custom implementation for loadUserByUsername
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> repository.findByEmail(username)
+                                     .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+
+}
