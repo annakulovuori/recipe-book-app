6d51474d731d2e8f3bd3b5dba8f194f6d0e68b07
Anna Kulovuori <anna.kulovuori@tuni.fi>
2024-10-17 17:13:13 +0300

    backend CREATE: new register and authenticate methods

--- backend/src/main/java/com/annakulovuori/recipeApp/services/AuthenticationService.java
+++ backend/src/main/java/com/annakulovuori/recipeApp/services/AuthenticationService.java
@@ -1,9 +1,18 @@
 package com.annakulovuori.recipeApp.services;
 
+import com.annakulovuori.recipeApp.security.AuthenticationRequest;
+import com.annakulovuori.recipeApp.security.AuthenticationResponse;
+import com.annakulovuori.recipeApp.security.RegisterRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AuthenticationService {
+    public AuthenticationResponse register(RegisterRequest request) {
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+    }
 }
+
