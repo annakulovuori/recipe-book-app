284fa05e6570f1e473d1ff6b4e12df93cb977e9e
Anna Kulovuori <anna.kulovuori@tuni.fi>
2024-10-17 14:13:28 +0300

    backend FEAT: Get userdetails from database. Update securityContextHolder if token is valid

--- backend/src/main/java/com/annakulovuori/recipeApp/security/JwtAuthenticationFilter.java
+++ backend/src/main/java/com/annakulovuori/recipeApp/security/JwtAuthenticationFilter.java
@@ -1,12 +1,17 @@
 package com.annakulovuori.recipeApp.security;
 
-import com.annakulovuori.recipeApp.service.JwtService;
+import com.annakulovuori.recipeApp.services.JwtService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -17,6 +22,7 @@ import java.io.IOException;
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtService jwtService;
+    private UserDetailsService userDetailsService;
 
     @Override                        // request, response and filter chain cannot be null
     protected void doFilterInternal(@NonNull HttpServletRequest request,
@@ -26,6 +32,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         final String authHeader = request.getHeader("Authorization");
         final String jwtToken;
         final String userEmail;
+
         if (authHeader == null || !authHeader.startsWith("Bearer ")) {
             filterChain.doFilter(request, response);
             return;
@@ -33,6 +40,25 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         // extract token from header starting after bearer
         jwtToken = authHeader.substring(7);
         userEmail = jwtService.extractUsername(jwtToken); // extact email from jwt token
+
+        // if username isn't null and there isn't authentication already
+        if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            // get userdetails from database
+            UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
+
+            // if before and token is valid update securityContext
+            if (jwtService.isTokenValid(jwtToken, userDetails)) {
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                        userDetails,
+                        null,
+                        userDetails.getAuthorities()
+                );
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+        }
+        // go to next filter
+        filterChain.doFilter(request, response);
 
     }
 }
