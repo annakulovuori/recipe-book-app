d81a54bc343a12893634539069473ca5d32fc6a5
Anna Kulovuori <anna.kulovuori@tuni.fi>
2024-10-17 15:06:28 +0300

    backend CREATE: new bean securityFilterChain with HttpSecurity parameter. FEAT: setup filterChain

--- backend/src/main/java/com/annakulovuori/recipeApp/config/SecurityConfig.java
+++ backend/src/main/java/com/annakulovuori/recipeApp/config/SecurityConfig.java
@@ -1,19 +1,43 @@
 package com.annakulovuori.recipeApp.config;
 
+import com.annakulovuori.recipeApp.security.JwtAuthenticationFilter;
+import jakarta.servlet.Filter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) {
+    private final JwtAuthenticationFilter jwtAuthFilter;
+    private final AuthenticationProvider authenticationProvider;
 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        // configuring endpoints that need and don't need authorization
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("")// these have access without authentication
+                        .permitAll()
+                        .anyRequest()// any other need authentication
+                        .authenticated()
+                )
+                .sessionManagement(session -> session
+                        // Every request is a new session so securityContextHolder doesn't store long
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class); // this filter before usernamePasswordAuthenticationFilter
+
+        return http.build();
     }
 }
