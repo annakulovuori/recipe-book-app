9c8d42f74ec76c8a624e5892e1b34a220b6c4e43
Anna Kulovuori <anna.kulovuori@tuni.fi>
2024-10-18 12:13:32 +0300

    backend REFACTOR: change secret_key to longer one

--- backend/src/main/java/com/annakulovuori/recipeApp/services/JwtService.java
+++ backend/src/main/java/com/annakulovuori/recipeApp/services/JwtService.java
@@ -16,7 +16,7 @@ import java.util.function.Function;
 @Service
 public class JwtService {
 
-    private static final String SECRET_KEY = "secret_key";
+    private static final String SECRET_KEY = "ccd87be86ec6814fa303fd1e97faf9a29ee54f227a57a45f07c074f2f2138d3ee1786daba6bdba58b9edefbcd7cc16ee993759c9b79cdfd55a1d15c0a8db8c11276bcccdb5be5d0cccd9d20c7407084398e7b8b3dd19e405f9b474253593a630d5989952b9ed390a1059438e4da4be1a7b220e1ad2f85657c21071f6313e5bae82bcf5db654b8b093a65b531b947c1cbf85dcc88e6356c90f8f419f4f011ccc848fb3af80cb3969773ecf1fe33a918c6c2991ceff6c70e9f2ca3c3aee266bfcd88c6dae5cf8dc1fbcafe22dad5fbff6876886802ce03ca7fd6fcabdfac7e4ee35428cee172a97be00ad9c06b3d6e63d0fb04a4ab558bf5da0dfa91a8a81ebc32";
 
     // generate token without extra claims, using generate token with extra claims
     public String generateToken(UserDetails userdetails) {
