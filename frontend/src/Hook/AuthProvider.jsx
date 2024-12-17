9d35d24997daa74f823b2f4e4a1a3bc642d945c0
Anna Kulovuori <anna.kulovuori@tuni.fi>
2024-12-17 14:50:48 +0200

    frontend CREATE: authenticate function

--- frontend/src/Hook/AuthProvider.jsx
+++ frontend/src/Hook/AuthProvider.jsx
@@ -1,24 +1,43 @@
 import { useContext, createContext, useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { jwtDecode } from "jwt-decode"
+import { jwtDecode } from "jwt-decode";
+import axios from "axios";
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-
   //default token state is empty
   const [token, setToken] = useState("");
   const [username, setUsername] = useState(null);
 
-//check if token exists and set values to state
+  //check if token exists and set values to state
   useEffect(() => {
-    const storedToken = localStorage.getItem("token")
+    const storedToken = localStorage.getItem("token");
     if (storedToken) {
-        setToken(storedToken)
-        const decodedToken = jwtDecode(storedToken)
-        setUsername(decodedToken.sub)
+      setToken(storedToken);
+      const decodedToken = jwtDecode(storedToken);
+      setUsername(decodedToken.sub);
     }
-  },[])
+  }, []);
+
+  const authenticate = async (email, password) => {
+    try {
+      const response = await axios.post(
+        "http://localhost:8080/api/auth/authenticate",
+        { email, password }
+      );
+      if (response.data.token) {
+        setToken(response.data.token);
+        const decodedToken = jwtDecode(response.data.token);
+        setUsername(decodedToken.sub);
+        localStorage.setItem("token", response.data.token);
+        return response.data;
+      }
+      throw new Error("Authentication failed, cannot find token");
+    } catch (error) {
+      throw new Error("Error during authentication");
+    }
+  };
 
   return (
     <AuthContext.Provider
