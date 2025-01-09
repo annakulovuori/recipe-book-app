1065fa4cfc6aa06fe6f9dfcbe2a6a60d3b2e8bfc
Anna Kulovuori <anna.kulovuori@tuni.fi>
2025-01-09 13:19:21 +0200

    frontend FEAT: use router to navigate pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,25 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import Login from "./pages/Login"
-import Register from "./pages/Register"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Profile from "./pages/Profile";
+import { AuthProvider } from "./Hook/AuthProvider";
 
 export default function App() {
   return (
     <Router>
-      <AppLayout />
+      <AuthProvider>
+        <AppLayout />
+      </AuthProvider>
     </Router>
-  )
+  );
 }
 
 function AppLayout() {
   return (
     <Routes>
-      <Route path="/"
-      element={
-        <Login />
-      }
-      />
-      <Route path="/register"
-      element={
-        <Register />
-      }
-      />
+      <Route path="/" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/profile" element={<Profile />} />
     </Routes>
   );
 }
