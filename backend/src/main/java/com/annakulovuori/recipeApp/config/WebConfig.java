7e723271043be2d81fa1d09ad2110199b5325d5f
Anna Kulovuori <anna.kulovuori@tuni.fi>
2025-01-09 13:19:53 +0200

    backend tilpehööri

--- backend/src/main/java/com/annakulovuori/recipeApp/config/WebConfig.java
+++ backend/src/main/java/com/annakulovuori/recipeApp/config/WebConfig.java
@@ -1,2 +1,20 @@
-package com.annakulovuori.recipeApp.config;public class WebConfig {
+package com.annakulovuori.recipeApp.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebSecurity
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173", "http://localhost:8080")
+                .allowedMethods("GET", "POST", "PATCH", "DELETE", "PUT")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
 }
